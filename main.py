9e7d16a664ce2d02ab994e39edea52e2989a685e
Smokey 8 <jacobramsdell316@gmail.com>
2023-06-24 17:18:11 -0700

    Assignment 1: Average Students

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
-
+    num_males = int(input('Enter number of male students: '))
+    num_females = int(input('Enter number of female students: '))
+    total = num_males + num_females
+    m_perc = (num_males / total) *100
+    f_perc = (num_females / total) *100
+    print(num_males)
+    print(num_females)
+    print(total)
+    print(f'Percentage of male students \t {m_perc: .2f}')
+    print(f'Percentage of female students \t {f_perc: .2f}')
+    
     """
     ########################################
     # Do not delete the return statement
